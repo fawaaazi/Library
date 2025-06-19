3d8f9335366dab09c070ae92b8cc37adcebbd1cf
fawaaazi <muhammedfawas90@gmail.com>
2025-06-20 01:08:54 +0530

    completed the ui portion almost buttons are yet to be added to the book card

---
+++ script.js
@@ -0,0 +1,32 @@
+const myLibrary = [];
+
+function Book(title, author, pages){
+    this.title = title ;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary() {
+    const title = prompt("Enter the title");
+    const author = prompt("Enter the author")
+    const pages = prompt("Enter pages");
+    const book = new Book(title, author, pages)
+    createBookCard(book);
+}
+
+function createBookCard(book){
+    const cardGrid = document.querySelector('.book-grid');
+    const div = document.createElement("div")
+    const title = document.createElement("p")
+    const author = document.createElement("p")
+    const pages = document.createElement("p")
+    title.textContent = book.title
+    div.appendChild(title)
+    author.textContent = book.author
+    div.appendChild(author)
+    pages.textContent = book.pages
+    div.appendChild(pages)
+    div.className = "book-card"
+    cardGrid.appendChild(div)
+
+}
